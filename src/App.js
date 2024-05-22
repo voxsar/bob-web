f874bdf9ef435d6b537b9e40bd45404f3c9f821f
PruthiY <Pruthivika@10XTEK.com>
2024-05-22 23:44:47 +0530

    Create basic structure and install strapi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './common/components/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Footer from './common/components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='bob-website'>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/common/components/Header.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import './styles/Header.css';
+
+const Header = () => {
+    return (
+       <>
+       <Navbar className='header-top-bar-light'>
+       <Container>
+       <Nav className="ml-auto header-social">
+            <Nav.Link href="#"><i className="fab fa-facebook-f"></i></Nav.Link>
+            <Nav.Link href="#"><i className="fab fa-instagram"></i></Nav.Link>
+            <Nav.Link href="#"><i className="fab fa-twitter"></i></Nav.Link>
+            <Nav.Link href="#"><i className="fab fa-youtube"></i></Nav.Link>
+            <Nav.Link href="#"><i className="fab fa-tiktok"></i></Nav.Link>
+            <Nav.Link href="#"><i className="fab fa-pinterest"></i></Nav.Link>
+          </Nav>
+          <Nav className="mr-auto header-contact">
+            <Nav.Link href="tel:320-639-2229">(320) 639-2229</Nav.Link>
+            <Nav.Link href="mailto:sales@babysonbroadway.com">sales@babysonbroadway.com</Nav.Link>
+          </Nav>
+         
+        </Container>
+       </Navbar>
+        <Navbar className="header-top-bar-dark" expand="lg">
+        <Container className="header-menu">
+          <Nav>
+            <Nav.Link href="#">Sign Up For Our Newsletter</Nav.Link>
+          </Nav>
+          <Nav className="ml-auto">
+          <Nav.Link href="#"><i className="fas fa-award"></i>Loyalty Points</Nav.Link>
+            <Nav.Link href="#"><i className="fas fa-gift"></i>Gift Cards</Nav.Link>
+            <Nav.Link href="#"><i className="fas fa-map-marker-alt"></i>Track Your Order</Nav.Link>
+            <Nav.Link href="#"><i className="fas fa-store"></i>Store Locations</Nav.Link>
+            <Nav.Link href="#"><i className="fas fa-user-circle"></i>Sign In</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+       </>
+    );
+}
+
+export default Header;
