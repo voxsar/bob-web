188742ede609be2647c9d691bfaaa8ccc5447c75
PruthiY <Pruthivika@10XTEK.com>
2024-06-06 12:25:28 +0530

    Fix overflow issues

--- src/common/components/header/headerNav.jsx
+++ src/common/components/header/headerNav.jsx
@@ -37,7 +37,6 @@ const HeaderNav = ({ isDesktop = true, toggleDrawer }) => {
                     <NavLink to="/blogs" className='nav-link' onClick={toggleDrawer}>BOB Defenders</NavLink>
                 </Nav.Item>
                 <Nav.Item>
-                    <NavLink to="/faqs" className='nav-link' onClick={toggleDrawer}>event spaces</NavLink>
                     <NavLink to="/event-spaces" className='nav-link' onClick={toggleDrawer}>event spaces</NavLink>
                 </Nav.Item>
                 <Nav.Item>
